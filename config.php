17870b5aef662cf0e0886f0298cc013ee90d5032
Radoslaw <radoslaw.mazur97@gmail.com>
2022-06-27 23:31:39 +0200

    .gitignore

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'lxqvrziddkjssl';
-const PASSWORD = '6a724a10fd6f5fa30cc7fdd200c3a52ee4ff6c626d3ed5f5e2da4cc803cc7769';
-const HOST = 'ec2-99-81-137-11.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd3qe917bnvbuf';
